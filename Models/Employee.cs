6c0429eccce73b5a0feebb6178368054c88254b5
harizshah <harizsharizad@gmail.com>
2025-10-31 11:54:17 +0800

    done

---
+++ Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace HarizLearnRouting.Models
+
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, string position, double salary)
+        {
+            Id = id;
+            Name = name;
+            Position = position;
+            Salary = salary;
+        }
+    }
+
+}
